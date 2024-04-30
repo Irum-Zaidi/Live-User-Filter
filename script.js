95bdbe16b2e1c21ad2cdf307ae40ee529b5ec2b9
Irum-Zaidi <91278807+Irum-Zaidi@users.noreply.github.com>
2024-04-30 13:31:38 +0500

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ getData()
 filter.addEventListener('input', (e) => filterData(e.target.value))
 
 async function getData() {
-    const res = await fetch('https://randomuser.me/api?results=50')
+    const res = await fetch('https://randomuser.me/api?results=30')
     // const data = await res.json()
     const { results } = await res.json()
 
